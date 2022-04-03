afc14cf6a47797bcab966878036e8a057b71fdc7
Lukasz Grzesik <lukasz.grzesik@nexthink.com>
2022-04-03 22:38:40 +0200

    Update readme.

--- user-event-enricher/src/main/java/com/github/simplesteph/udemy/kafka/streams/UserDataProducer.java
+++ user-event-enricher/src/main/java/com/github/simplesteph/udemy/kafka/streams/UserDataProducer.java
@@ -19,7 +19,7 @@ public class UserDataProducer {
         Properties properties = new Properties();
 
         // kafka bootstrap server
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9094");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         // producer acks
